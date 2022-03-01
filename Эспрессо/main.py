4731a5ca8113765476442d55a4a0c94d60b96042
Никита <nikitaivanov067@gmail.com>
2022-03-01 14:40:28 +0300

    Эспрессо

---
+++ Эспрессо/main.py
@@ -0,0 +1,41 @@
+import sqlite3
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+from PyQt5.QtGui import QIcon
+
+
+class DBSample(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.setWindowIcon(QIcon("my_icon.png"))
+
+        self.connection = sqlite3.connect("coffee.sqlite")
+
+        res = self.connection.cursor().execute("SELECT * FROM coffees").fetchall()
+        # Заполним размеры таблицы
+        self.tableWidget.setColumnCount(5)
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setWordWrap(True)
+
+        # Заполняем таблицу элементами
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+
+    def closeEvent(self, event):
+        # При закрытии формы закроем и наше соединение
+        # с базой данных
+        self.connection.close()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = DBSample()
+    ex.show()
+    sys.exit(app.exec())
